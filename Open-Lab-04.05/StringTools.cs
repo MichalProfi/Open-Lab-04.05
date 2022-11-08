12a5666d376c2495bdf013bf06ab641a4a66aa52
Michal Holena <113421705+MichalProfi@users.noreply.github.com>
2022-11-08 08:29:17 +0100

    add - a function that can repeat string letters

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string modified = String.Empty;
+            for (int i = 0; i < orig.Length; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    modified += orig[i];
+            }
+            return modified;
         }
     }
 }
